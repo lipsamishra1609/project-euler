bb43d849620ae906f5ee45adde924b9b3b3e1b56
Lipsa Mishra <lipsamishra1609@gmail.com>
2020-07-31 20:54:26 +0530

    Create prog3.java

---
+++ prog3.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class prog3 {
+
+public static boolean Ispalindrome(long n)	
+{     
+	  long sum=0;
+	  long cpy=n;
+      while(n>0)
+      {
+    	  long d=n%10;
+    	  sum=sum*10+d;
+    	  n=n/10;
+      }
+      if(cpy==sum)
+      {
+    	  return true;
+      }
+      else
+      {
+    	  return false;
+      }
+
+}
+	    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//System.out.println("enter the number");
+		//long n=sc.nextLong();
+		long mul;
+		long lar=0;
+		for(int i=100;i<=999;i++)
+		{
+			for(int j=100;j<=999;j++)
+			{
+			mul=i*j;
+			if(Ispalindrome(mul)==true)
+			{
+				lar=mul;
+			}
+			}
+		}
+		System.out.println(lar);
+
+	}
+
+}
