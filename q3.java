f53564a46671e0bd7bc0fa747606c062b64a86b2
Lipsa Mishra <lipsamishra1609@gmail.com>
2020-09-12 13:17:16 +0530

    Create q3.java

---
+++ q3.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class prog3 {
+
+	public static boolean IsPrime(long n)
+	{
+		int flag=1;
+		for(long i=2;i*i<=n;i++)
+	    {
+	  	  if (n%i==0)
+	  	  {
+	  		 flag=0;
+	  		 break;
+	  	  }
+	  	 
+		  }
+	    if (flag==1)
+	  		  {
+	  	  return true;
+	  	  }
+	    else
+	    {
+	  	  return false;
+	    }
+	 }
+	
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+	        long n=600851475143L;
+	        long max=0;
+	        for (long i=2;i*i<=n;i++)
+	        {
+	        	if(n%i==0)
+	        	{
+	        		if(IsPrime(i))
+	        		{
+	        	       if(i>max)
+	        	      {
+	        	    	max=i;
+	        	      }
+	                }
+	             }
+	         }
+	        System.out.println("maximum prime factor "+max);
+	     }
+}
