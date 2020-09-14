ef9b0caca7332b965cc8cd45f10fc542ff077df9
Lipsa Mishra <lipsamishra1609@gmail.com>
2020-09-14 11:59:43 +0530

    Update q10.java

--- q10.java
+++ q10.java
@@ -1,27 +1,36 @@
 //Find the sum of all the primes below two million.
-public class q10 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       long sum=2;
-	   for(long n=3;n<2000000;n++)
+public class prog10 {
+	public static boolean isPrime(long n)
+	{
+		int flag=1;
+		for(int i=2;i*i<=n;i++)
 		{
-		   int check=0;
-	       for(long i=2;i*i<=n;i++)
-           {
-			  if(n%i==0)
-			  {
-				check=1;
+			if(n%i==0)
+			{
+				flag=0;
 				break;
-			  } 
-	       }
-	       if(check==0)
-	       {
-	    	   sum=sum+n;
-	       }
+			}
 		}
-		System.out.println(sum);
+		if(flag==1)
+		{
+			return true; 
+		}
+		else
+		{
+			return false;
+		}
 	}
-
+          public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            long sum=2;
+	    for(long n=3;n<2000000;n++)
+		{
+		   if(isPrime(n))
+		   {
+	    	   sum=sum+n;
+	           }
+		}
+	    System.out.println(sum);
+	}
 }
 //output: 142913828922
