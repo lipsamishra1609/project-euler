85c153c8c233597cc502fc1b0b620798de9e38b0
Lipsa Mishra <lipsamishra1609@gmail.com>
2020-09-15 10:42:05 +0530

    Update q6.java

--- q6.java
+++ q6.java
@@ -1,5 +1,5 @@
 //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-public class prog6 {
+public class q6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
