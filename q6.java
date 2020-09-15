eecc0db11168dff2e21ac3e35c7c90ebd00eeaec
Lipsa Mishra <lipsamishra1609@gmail.com>
2020-09-15 10:25:39 +0530

    Update q6.java

--- q6.java
+++ q6.java
@@ -1,16 +1,14 @@
 //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-public class q6 {
+public class prog6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        int sum1=0;
        int sum2=0;
-       for(int i=1;i<=100;i++)
-       {   
-    	   sum1=sum1 + (i*i);
-    	   sum2=sum2 + i;
-       }
-       System.out.println((sum2*sum2)-sum1);
+       int n=100;
+       sum1 = n * (n+1)/ 2;
+       sum2 = (n * (n + 1) * (2 * n + 1)) / 6;
+       System.out.println("the difference is : "+((sum1*sum1)-sum2));
        
 	}
 
