94ccd9a46a2a7e571c7ab82265e6ac79009e9cec
Lipsa Mishra <lipsamishra1609@gmail.com>
2020-09-15 21:29:22 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -3,14 +3,14 @@ public class q3 {
 	public static void main(String[] args) {
 	  // TODO Auto-generated method stub
 		long n=600851475143L;
-	        long i=2;
+	        long i=3;
 	        while(i<=Math.sqrt(n))
 	        {
 	        	if(n%i==0)
 	        	{
 	        		n=n/i;
 	        	}
-	        	i++;
+	        	i=i+2;
 	        }
 	        System.out.println("Largest prime factor is : "+n);
 	     }
