bcf91550f2b7c7ea2a8a6e5cbfccc2fbaf34c269
Lipsa Mishra <lipsamishra1609@gmail.com>
2020-09-15 22:10:48 +0530

    Update q7.java

--- q7.java
+++ q7.java
@@ -1,36 +1,37 @@
-//What is the 10 001st prime number?
-public class q7 {
-
-	public static void main(String[] args) {
+//What is the 10001st prime number?
+public class prog7 {
+    public static boolean isPrime(long n)
+	{
+		if(n<2)
+		{
+			return false;
+		}
+		if(n%2==0)
+		{
+			return false;
+		}
+		for(int i=3;i<=Math.sqrt(n);i++)
+		{
+			if(n%i==0)
+			{
+		      return false;
+			}
+		}
+		return true;
+	}
+    public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     int count=0,l=0;	
-	 long primeNumber=0;
-     long i=2;
-     while(i>0)
+     int count=1;	
+	 long primeNumber=2;
+     while(count<10001)
      {
-    	 for(long j=1;j*j<=i;j++)
-    	 {
-    		 if(i%j==0)
-    		 {
-    			 l++;
-    		 }
-      	 }
-    	 if(l==1)
+    	 primeNumber++;
+    	 if(isPrime(primeNumber))
     	 {
     		 count++;
     	 }
-    	 if(count==10001)
-    	 {
-    		 primeNumber=i;
-    		 break;
-    	 }
-    	 i++;
-    	 l=0;
      }
-
-      System.out.println("10001st prime number is : "+primeNumber);
+     System.out.println("10001st prime number is : "+primeNumber);
 	}
-
 }
-
 //output: 104743
