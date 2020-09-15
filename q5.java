588dc52cd97a73f34878a0c8c6374669bff2d7e4
Lipsa Mishra <lipsamishra1609@gmail.com>
2020-09-15 12:56:10 +0530

    Update q5.java

--- q5.java
+++ q5.java
@@ -1,26 +1,32 @@
 //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 public class q5 {
-   public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        int number=0,n=2520,i=2;
-        while(i<=20)
-        {
-           if(n%i==0)
-           {
-        	   if(i==20)
-        	   {
-        	   number=n;
-        	   break;
-        	   }
-        	   i++;
-           }
-           else
-              {
-        	   n++;
-        	   i=1;
-              }
-        }
-        System.out.println("the smallest integer divisible : "+number);
-     }
+	
+	public static long gcd(long a, long b)
+	{
+		long gcd=1;
+		for(long i=1;i<=a&&i<=b;i++)
+		{
+			if(a%i==0&&b%i==0)
+			{
+				gcd=i;
+			}
+		}
+		return gcd;
+	}
+	
+	public static long lcm(long n)
+	{
+		long multiple=2520;
+		for(long i=11;i<=n;i++)
+			multiple=(multiple*i)/gcd(multiple,i);
+		    return multiple;
+	  	
+	}
+	
+	public static void main(String[]args)
+	{
+		long number=20;
+		System.out.println(lcm(number));
+	}
 }
 //output: 232792560
