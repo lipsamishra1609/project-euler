fb2d857c8550dc7f44244c6db4ef79869575f8ae
Lipsa Mishra <lipsamishra1609@gmail.com>
2020-09-16 15:12:37 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -1,18 +1,21 @@
 //What is the largest prime factor of the number 600851475143 
 public class q3 {
 	public static void main(String[] args) {
-	  // TODO Auto-generated method stub
-		long n=600851475143L;
-	        long i=3;
-	        while(i<=Math.sqrt(n))
+	// TODO Auto-generated method stub
+	        long n=600851475143L;
+	        long i=2;
+	       while(n!=1)
 	        {
 	        	if(n%i==0)
 	        	{
-	        		n=n/i;
+	        	   n=n/i;
 	        	}
-	        	i=i+2;
+	        	else {
+	        		i++;
+	        	}
 	        }
-	        System.out.println("Largest prime factor is : "+n);
+	        System.out.println("Largest prime factor is : "+i);
 	     }
 }
+
 // output : 6857
